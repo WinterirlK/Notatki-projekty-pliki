5bcf6efd2586d40b6dd2f1f04102e21fce2938d0
krystalon133 <126268514+krystalon133@users.noreply.github.com>
2024-01-03 17:22:48 +0100

    Update sito_erastotenesa_listy.cpp

--- sito_erastotenesa_listy.cpp
+++ sito_erastotenesa_listy.cpp
@@ -29,23 +29,39 @@ void Druk(struct Lista * Head){
     }
 }
 
-struct Lista * Del(struct Lista * Head){
-    Head=Head->Next;
+int count(struct Lista * Head){
+    int i = 0;
+    for(;Head!=NULL;i++,Head=Head->Next);
+    return i;
+}
+
+struct Lista*Del(struct Lista * Head){
+    struct Lista *p = Head->Next;
+    Head->Next=Head->Next->Next;
+    free(p);
     return Head;
 }
 
 struct Lista * SitoErastotenesa(int n){
-    struct Lista * Head = NULL;
+    struct Lista *Head = NULL;
     for(int i = 2;i<=n;i++){
         Head = Insert(Head,i);
     }
-    struct Lista * temp = Head;
-    for(int i = 2;i<=n;i++){
-        temp = Head;
+    struct Lista * tmp = Head;
+    for(int i=2;i*i<=n;i++){
+        tmp=Head;
+        while (tmp->liczba<i){
+            tmp=tmp->Next;
+        }
         for(int j = i+i;j<=n;j+=i){
-            while (temp!=NULL){
-                if(temp->liczba==j) temp = Del(Head);
-                temp=temp->Next;
+            while (tmp->Next!=NULL && tmp->Next->liczba<j){
+                tmp=tmp->Next;
+            }
+            if(tmp->Next!=NULL && tmp->Next->liczba>j){
+                continue;
+            }
+            else if(tmp->Next!=NULL){
+                tmp=Del(tmp);
             }
         }
     }
@@ -53,7 +69,23 @@ struct Lista * SitoErastotenesa(int n){
 }
 
 int main(){
-    struct Lista * Head = SitoErastotenesa(10);
-    Druk(Head);
+    struct Lista* tab = SitoErastotenesa(100000);
+    struct Lista* tab2;
+    int i, tmp;
+
+    for (i=2; i<=100000; i++){
+        printf("%d -", i);
+        tmp = i;
+        while(tmp > 1){
+            for (tab2 = tab; ; tab2 = tab2->Next){
+                if ((tmp % tab2->liczba) == 0){
+                    tmp /= tab2->liczba;
+                    printf(" %d", tab2->liczba);
+                    break;
+                }
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
