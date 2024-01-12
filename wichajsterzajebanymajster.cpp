d3fbc5f5f41a617d4dc52cead163ef3a2d771964
krystalon133 <126268514+krystalon133@users.noreply.github.com>
2024-01-13 00:30:47 +0100

    Create wichajsterzajebanymajster.cpp

---
+++ wichajsterzajebanymajster.cpp
@@ -0,0 +1,124 @@
+#include <cstdio>
+#include <cstdbool>
+
+bool Left_to_right = true;
+bool Right_to_left = false;
+
+unsigned long long int silnia(int n);
+int Permutacja(int size, int k);
+int zmieszanie(int *A, int size);
+int FindMaxMobileElement(int * A, bool * arrow, int size);
+void change_arrow(int *A, int zmieniany, bool * arrow, int size);
+
+//18446744073709551615
+//silnia 65-max
+int main() {
+    unsigned long long  int wynik = ((Permutacja(3,2)/silnia(3))%1000000007);
+    printf("%llu", wynik);
+}
+
+unsigned long long int silnia(int n){
+    if(n<3){
+        return n;
+    }
+    else{
+        unsigned long long int wynik = n;
+        for(unsigned long long int i = 2;i<n;i++){
+            wynik *= i;
+        }
+        return wynik;
+    }
+}
+//rl false
+//lr true
+int Permutacja(int size, int k){
+    int A[size];
+    bool arrow[size];
+    int ilosc = 0;
+    for(int i = 1; i<=size; i++){
+        A[i-1]=i;
+        arrow[i-1] = Right_to_left;
+    }
+    if(k==zmieszanie(A,size))ilosc++;
+    unsigned long long int liczba_permutacji = silnia(size)-1;
+    int tmp1, tmp2;
+    bool tmp3;
+    int zmieniany;
+    for (int i = 0; i < liczba_permutacji; i++) {
+        tmp1 = FindMaxMobileElement(A,arrow,size);
+        zmieniany = A[tmp1];
+        if(arrow[tmp1]){
+            tmp2 = A[tmp1+1];
+            A[tmp1+1] = A[tmp1];
+            A[tmp1] = tmp2;
+            tmp3 = arrow[tmp1+1];
+            arrow[tmp1+1] = arrow[tmp1];
+            arrow[tmp1] = tmp3;
+        }
+        else{
+            tmp2 = A[tmp1-1];
+            A[tmp1-1] = A[tmp1];
+            A[tmp1] = tmp2;
+            tmp3 = arrow[tmp1-1];
+            arrow[tmp1-1] = arrow[tmp1];
+            arrow[tmp1] = tmp3;
+        }
+        change_arrow(A,zmieniany,arrow,size);
+        if(k==zmieszanie(A,size))ilosc++;
+    }
+
+
+    return ilosc;
+}
+
+void change_arrow(int *A, int zmieniany, bool * arrow, int size){
+    for(int i =0;i<size;i++){
+        if(zmieniany<A[i]){
+            if(arrow[i])arrow[i]= false;
+            else arrow[i] = true;
+        }
+    }
+}
+
+int FindMaxMobileElement(int * A, bool * arrow, int size){
+    int id = 0;
+    int najwiekszy = 0;
+    for(int i = 0;i<size;i++){
+        if(arrow[i]){
+            if(i<size-1 && A[i]>A[i+1]){
+              if(A[i]>najwiekszy){
+                  najwiekszy = A[i];
+                  id = i;
+              }
+            }
+        }
+        else{
+            if(i>0 && A[i]>A[i-1]){
+                if(A[i]>najwiekszy){
+                    najwiekszy = A[i];
+                    id = i;
+                }
+            }
+        }
+    }
+    return id;
+}
+
+int zmieszanie(int *A, int size){
+    int k=1;
+    int B[size];
+    for(int i = 0;i<size;i++){
+        B[i] = A[i];
+    }
+    etykieta:
+        for(int i = 0;i<size;i++){
+            B[i] = A[B[i]-1];
+        }
+    for(int i =0;i<size;i++){
+        if(A[i] != B[i]){
+            k++;
+            goto etykieta;
+        }
+    }
+    return k;
+}
